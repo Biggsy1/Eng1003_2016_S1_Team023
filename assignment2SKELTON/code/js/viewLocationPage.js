612d1a76837575ffec4a1f06d0b5d47ec336a853
Bray Morrow <BrayMorrow@Brays-MacBook-Pro.local>
2016-05-11 11:58:48 +1000

    Establishing the Map on Location Page

--- assignment2SKELTON/code/js/viewLocationPage.js
+++ assignment2SKELTON/code/js/viewLocationPage.js
@@ -10,3 +10,28 @@ if (locationIndex !== null)
     // If a location name was specified, use it for header bar title.
     document.getElementById("headerBarTitle").textContent = locationNames[locationIndex];
 }
+
+
+
+window.onload = function initMap()
+{
+    // Display a map, centred on their location.
+    var usersChosenLocation = {lat: LATITUDE of their place, lng: LONGITUDE of their place};
+    var map = new google.maps.Map(document.getElementById("map"), {
+            // CAREFUL with zoom!!!
+            zoom: 16,
+            center: usersChosenLocation
+    });
+ 
+    // Display an overlay with a location pin and label.
+    var usersChosenLocation = {lat: LATITUDE of their place, lng: LONGITUDE of their place};
+    var infowindow = new google.maps.InfoWindow;
+    var marker = new google.maps.Marker({
+            position: usersChosenLocation,
+            map: map
+    });
+    infowindow.setContent("NAME ON MARKER");
+    infowindow.open(map, marker);
+}
+ 
+ 
