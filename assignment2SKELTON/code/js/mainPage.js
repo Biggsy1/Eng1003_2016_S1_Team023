9961e9d20f1a5530d53b25cea22859843a53e57c
Bray Morrow <BrayMorrow@dyn-118-138-3-74.its.monash.edu.au>
2016-05-16 17:30:38 +1000

    Call the code from the IndexPage And creates the list in Index page
    However, the list disappears after stepping through the code far enough

--- assignment2SKELTON/code/js/mainPage.js
+++ assignment2SKELTON/code/js/mainPage.js
@@ -2,7 +2,7 @@
 
 // This is sample code to demonstrate navigation.
 // You need not use it for final app.
-
+/*
 function viewLocation(locationName)
 {
     // Save the desired location to local storage
@@ -10,19 +10,70 @@ function viewLocation(locationName)
     // And load the view location page.
     location.href = 'viewlocation.html';
 }
+*/
 
 
+
+
+    // ADDED THIS!!!!
+    updateMainList();
+    
+/*
+    window.location = "viewlocation.html";
+    viewLocation(locationIndex); 
+*/
+
+
+
+
+function viewLocation(locationIndex){
+    
+    var locationsArray = loadLocations(); 
+
+    // Use the location name for header bar title.
+    document.getElementById("headerBarTitle").textContent = locationsArray[locationIndex].nickname;
+
+    initialiseMap(locationsArray, locationIndex);
+
+};
+
+
+
+function initialiseMap(locationsArrayToInitialiseMap, Index)
+{
+    // Display a map, centred on their location.
+    var usersChosenLocation = {lat: locationsArrayToInitialiseMap[Index].latitude, lng: locationsArrayToInitialiseMap[Index].longitude};
+    var map = new google.maps.Map(document.getElementById("map"), {
+            // CAREFUL with zoom!!!
+            zoom: 12,
+            center: usersChosenLocation
+    });
+ 
+    
+    // Display an overlay with a location pin and label.
+    var usersChosenLocation = {lat: locationsArrayToInitialiseMap[Index].latitude, lng: locationsArrayToInitialiseMap[Index].longitude};
+    var infowindow = new google.maps.InfoWindow;
+    var marker = new google.maps.Marker({
+            position: usersChosenLocation,
+            map: map
+    });
+    infowindow.setContent(locationsArrayToInitialiseMap[Index].nickname);
+    infowindow.open(map, marker);
+}
+ 
 // Source: 6.2 Alexandria Material
 // Needs to add the location to the main page when the location is stored in local Storage
 // May require a window.onload to update their blank page when they initially load???
 
 // window.onload = ready;
-
-
                  
-   // Needs access to the locations array! Check Weather(0)! Check viewLocation(0)!
-function updateMainList(locationIndex, accessToLocationsArray)
+   // Needs access to the locations array!
+function updateMainList()
 {
+    var locationsArray = loadLocations();
+    
+    for (i = 0; i < locationsArray.length; i++){
+    
     var tempLiNode, tempPrimarySpanNode;
     var tempImgNode, tempHeadingSpanNode, tempSummarySpanNode;
     var listOfLocations = document.getElementById("locationList");
@@ -30,29 +81,29 @@ function updateMainList(locationIndex, accessToLocationsArray)
     // Create an individual 'li' node to be a row for the location
     tempLiNode = document.createElement("li");
     tempLiNode.setAttribute("class", "mdl-list__item mdl-list__item--two-line");
-    tempLiNode.setAttribute("id", "location" + locationIndex);
-    tempLiNode.onclick = viewLocation(locationIndex);
+    tempLiNode.setAttribute("id", "location" + i);
+    tempLiNode.setAttribute("onclick", "viewLocation(" + i + ");");    
     
     // Create main span node for the primary content
     tempPrimarySpanNode = document.createElement("span");
     tempPrimarySpanNode.setAttribute("class", "mdl-list__item-primary-content");
-    tempPrimarySpanNode.setAttribute("id", "mainSpan" + locationIndex);
+    tempPrimarySpanNode.setAttribute("id", "mainSpan" + i);
     
     // Create image node
     tempImgNode = document.createElement("img");
     tempImgNode.setAttribute("class", "mdl-list__item-icon");
-    tempImgNode.setAttribute("id", "icon" + locationIndex);
+    tempImgNode.setAttribute("id", "icon" + i);
             // Src will need to change!!!
     tempImgNode.setAttribute("src", "images/loading.png");
     tempImgNode.setAttribute("class", "list-avatar");
     
     // Create Span node for 'heading'
     tempHeadingSpanNode = document.createElement("span")
-    tempHeadingSpanNode.textContent = accessToLocationsArray[locationIndex].nickname;
+    tempHeadingSpanNode.textContent = locationsArray[i].nickname;
     
     // Create span node for the weather summary
     tempSummarySpanNode = document.createElement("span");
-    tempSummarySpanNode.setAttribute("id", "weather" + locationIndex);
+    tempSummarySpanNode.setAttribute("id", "weather" + i);
     tempSummarySpanNode.setAttribute("class", "mdl-list__item-sub-title");
     
     /*
@@ -64,20 +115,21 @@ function updateMainList(locationIndex, accessToLocationsArray)
     */
     
     // Adds the 'tempLiNode' to the end of the listOfLocations (ul part)
-    listOfLocations.add(tempLiNode, null);
+    listOfLocations.appendChild(tempLiNode, null);
     
     // Makes a reference to this newly created li node
-    var liNode = document.getElementById("location" + locationIndex);
+    var liNode = document.getElementById("location" + i);
     // Adds directly to this node
-    liNode.add(tempPrimarySpanNode, null);
+    liNode.appendChild(tempPrimarySpanNode, null);
     
     // Makes a reference to this newly created primary span node
-    var primarySpanNode = document.getElementById("mainSpan" + locationIndex);
+    var primarySpanNode = document.getElementById("mainSpan" + i);
     // Adds directly to this node
-    primarySpanNode.add(tempImgNode, null);
-    primarySpanNode.add(tempHeadingSpanNode, null);
-    primarySpanNode.add(tempSummarySpanNode, null);
+    primarySpanNode.appendChild(tempImgNode, null);
+    primarySpanNode.appendChild(tempHeadingSpanNode, null);
+    primarySpanNode.appendChild(tempSummarySpanNode, null);
 
+    }
 };
 
 
